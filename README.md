e6369d65b878e7d956515caffd57b0a10a50939c
Kitson Kelly <me@kitsonkelly.com>
2022-05-20 10:30:35 +1000

    docs: improve README, add inline docs (#12)

--- README.md
+++ README.md
@@ -1,43 +1,81 @@
-# Deno.emit in user-space
+# emit
 
-In Deno 1.22, the unstable API `Deno.emit` was removed. This module provides a
-userspace implementation of it.
+[![deno doc](https://doc.deno.land/badge.svg)](https://doc.deno.land/https://deno.land/x/emit/mod.ts)
+[![Twitter handle][]][Twitter badge]
+[![Discord Chat](https://img.shields.io/discord/684898665143206084?logo=discord&style=social)](https://discord.gg/deno)
 
-https://deno.land/x/emit
+Transpile and bundle JavaScript and TypeScript in Deno and Deno Deploy.
 
+> This is an unstable module, where the API is likely to change over time.
 
-## Transpile
+## Why was `Deno.emit()` removed from Deno?
+
+In Deno 1.22 the unstable API `Deno.emit()` was removed. This module provides
+similar functionality as an optional user loaded module.
+
+Having any APIs that are platform specific (e.g. in the `Deno` namespace) makes
+code a lot less portable. We have generally avoided adding APIs to the `Deno`
+namespace if we can. While `Deno.emit()` and similar APIs have been part of the
+unstable features for an extended period of time, creating
+[deno_emit](https://deno.land/x/deno_emit),
+[deno_doc](https://deno.land/x/deno_doc) and
+[deno_cache](https://deno.land/x/deno_cache), which expose some of the internal
+Deno APIs as user loadable modules has proven to be a good pattern for giving
+the command line power of the Deno CLI to build end-user solutions, which also
+then can work on edge compute like [Deno Deploy](https://deno.com/deploy).
+
+So while taking away functionality from the Deno CLI may seem like a strange
+decision, by moving it to a user loadable module, it provides users more choice
+and flexibility.
+
+> Note: the examples below use "unpinned" version of the module. Consider using
+> a "pinned" version (e.g. `https://deno.land/x/emit@0.0.1/mod.ts` instead.)
+
+## Transpiling
+
+Take individual modules that are JavaScript or TypeScript and emit them in a
+transpiled fashion. An example of taking some TypeScript and transpiling to
+JavaScript:
 
 ```ts
-import { emit } from "https://deno.land/x/emit@0.0.1/mod.ts";
-import {
-  assert,
-  assertEquals,
-  assertStringIncludes,
-} from "https://deno.land/std@0.138.0/testing/asserts.ts";
+import { emit } from "https://deno.land/x/emit/mod.ts";
 
 const url = new URL("./testdata/mod.ts", import.meta.url);
 const result = await emit(url.href);
 
-console.log(result);
-assertEquals(Object.keys(result).length, 1);
-const code = result[Object.keys(result)[0]];
-assert(code)
-assertStringIncludes(code, "export default function hello()");
+const { code } = result;
+console.log(code.includes("export default function hello()"));
 ```
 
 ## Bundle
 
+Take a root module and all its dependencies and emit a single JavaScript bundle.
+This is similar to the functionality provided by `deno bundle` on the Deno
+command line. An example:
 
 ```ts
-import { bundle } from "https://deno.land/x/emit@0.0.1/mod.ts";
+import { bundle } from "https://deno.land/x/emit/mod.ts";
 const result = await bundle(
-  "https://deno.land/std@0.113.0/examples/chat/server.ts",
+  "https://deno.land/std@0.140.0/examples/chat/server.ts",
 );
-console.log(result);
-assert(result.code);
+
+const { code } = result;
+console.log(code);
 ```
 
-## Type-check
+## Type checking
 
-TODO
+Currently _emit_ does not perform type checking, while the prior `Deno.emit()`
+could. Getting type checking to run under Deno is a complex process. While we
+want to provide a user loadable solution for this, it is unlikely that it will
+be provided as part of this module, or would exist as a separate loadable module
+as part of this one.
+
+---
+
+Copyright 2018-2022 the Deno authors. All rights reserved. MIT License.
+
+[Build Status - Cirrus]: https://github.com/denoland/deno_emit/workflows/ci/badge.svg?branch=main&event=push
+[Build status]: https://github.com/denoland/deno_emit/actions
+[Twitter badge]: https://twitter.com/intent/follow?screen_name=deno_land
+[Twitter handle]: https://img.shields.io/twitter/follow/deno_land.svg?style=social&label=Follow
--- mod.ts
+++ mod.ts
@@ -1,14 +1,51 @@
 // Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
 
+/** APIs to transpile and bundle JavaScript and TypeScript under Deno and Deno.
+ *
+ * It is a user loadable module which provides an alternative to the removed
+ * unstable `Deno.emit()` API.
+ *
+ * ### Example - Transpiling
+ *
+ * ```ts
+ * import { emit } from "https://deno.land/x/emit/mod.ts";
+ *
+ * const url = new URL("./testdata/mod.ts", import.meta.url);
+ * const result = await emit(url.href);
+ *
+ * const { code } = result;
+ * console.log(code.includes("export default function hello()"));
+ * ```
+ *
+ * ### Example - Bundling
+ *
+ * ```ts
+ * import { bundle } from "https://deno.land/x/emit/mod.ts";
+ * const result = await bundle(
+ *   "https://deno.land/std@0.140.0/examples/chat/server.ts",
+ * );
+ *
+ * const { code } = result;
+ * console.log(code);
+ * ```
+ *
+ * @module
+ */
+
 import { bundle as jsBundle, transpile } from "./lib/deno_emit.generated.js";
 
 import type { CacheSetting } from "https://deno.land/x/deno_cache@0.2.0/file_fetcher.ts";
 
+/** The output of the {@linkcode bundle} function. */
 export interface BundleEmit {
+  /** The bundles code as a single JavaScript module. */
   code: string;
+  /** An optional source map. */
   map?: string;
 }
 
+/** The response from a `load()` function. Used when providing a custom
+ * loader. */
 export interface LoadResponse {
   /** The string URL of the resource. If there were redirects, the final
    * specifier should be set here, otherwise the requested specifier. */
@@ -20,22 +57,38 @@ export interface LoadResponse {
   content: string;
 }
 
+/** Options which can be set when using the {@linkcode bundle} function. */
 export interface BundleOptions {
+  /** Allow remote modules to be loaded or read from the cache. */
   allowRemote?: boolean;
+  /** The cache root to use, overriding the default inferred `DENO_DIR`. */
   cacheRoot?: string;
+  /** The setting to use when loading sources from the Deno cache. */
   cacheSetting?: CacheSetting;
+  /** Compiler options which can be set when bundling. */
   compilerOptions?: CompilerOptions;
+  /** External modules which are not direct dependencies of the code which
+   * should be in scope when bundling. */
   imports: Record<string, string[]>;
+  /** Override the default loading mechanism with a custom loader. This can
+   * provide a way to use "in-memory" resources instead of fetching them
+   * remotely. */
   load?(
     specifier: string,
     isDynamic: boolean,
   ): Promise<LoadResponse | undefined>;
+  /** Should the emitted bundle be an ES module or an IIFE script. The default
+   * is `"module"` to output a ESM module. */
   type?: "module" | "classic";
 }
 
+/** Options which can be set when using the {@linkcode emit} function. */
 export interface EmitOptions {
+  /** Allow remote modules to be loaded or read from the cache. */
   allowRemote?: boolean;
+  /** The cache root to use, overriding the default inferred `DENO_DIR`. */
   cacheRoot?: string;
+  /** The setting to use when loading sources from the Deno cache. */
   cacheSetting?: CacheSetting;
   //compilerOptions?: CompilerOptions;
   //imports: Record<string, string[]>;
@@ -45,16 +98,56 @@ export interface EmitOptions {
 
 export interface CompilerOptions {
   checkJs?: boolean;
+  /** Determines if reflection meta data is emitted for legacy decorators or
+   * not.  Defaults to `false`. */
   emitDecoratorMetadata?: boolean;
   importsNotUsedAsValues?: string;
+  /** When set, instead of writing out a `.js.map` file to provide source maps,
+   * the source map will be embedded the source map content in the `.js` files.
+   *
+   * Although this results in larger JS files, it can be convenient in some
+   * scenarios. For example, you might want to debug JS files on a webserver
+   * that doesn’t allow `.map` files to be served. */
   inlineSourceMap?: boolean;
+  /** When set, the original content of the `.ts` file as an embedded string in
+   * the source map (using the source map’s `sourcesContent` property).
+   *
+   * This is often useful in the same cases as `inlineSourceMap`. */
   inlineSources?: boolean;
+  /** Controls how JSX constructs are emitted in JavaScript files. This only
+   * affects output of JS files that started in `.jsx` or `.tsx` files. */
   jsx?: "jsx" | "preserve";
+  /** Changes the function called in `.js` files when compiling JSX Elements
+   * using the classic JSX runtime. The most common change is to use `"h"` or
+   * `"preact.h"`. */
   jsxFactory?: string;
+  /** Specify the JSX fragment factory function to use when targeting react JSX
+   * emit with jsxFactory compiler option is specified, e.g. `Fragment`. */
   jsxFragmentFactory?: string;
+  /** Enables the generation of sourcemap files. */
   sourceMap?: boolean;
 }
 
+/** Generate a single file JavaScript bundle of the root module and its
+ * dependencies.
+ *
+ * ### Example
+ *
+ * ```ts
+ * import { bundle } from "https://deno.land/x/emit/mod.ts";
+ * const result = await bundle(
+ *   "https://deno.land/std@0.140.0/examples/chat/server.ts",
+ * );
+ *
+ * const { code } = result;
+ * console.log(code);
+ * ```
+ *
+ * @param root The root module specifier to use for the bundle.
+ * @param options Options to use when bundling.
+ * @returns a promise which resolves with the emitted bundle (and optional
+ *          source map)
+ */
 export async function bundle(
   root: string,
   options: BundleOptions = {},
@@ -83,6 +176,15 @@ export async function bundle(
   return jsBundle(root, bundleLoad, imports, undefined);
 }
 
+/** Transpile TypeScript (or JavaScript) into JavaScript, returning a promise
+ * which resolves with a map of the emitted files.
+ *
+ * @param root The root module specifier to use for the bundle.
+ * @param options Options to use when emitting.
+ * @returns A promise which resolves with an object map of the emitted files,
+ *          where the key is the emitted files name and the value is the
+ *          source for the file.
+ */
 export async function emit(
   root: string,
   options: EmitOptions = {},
