47605ee7f02a12c1c5bcf8d54aed834dd85defa6
David Sherret <dsherret@gmail.com>
2023-03-18 22:31:53 -0400

    Support json modules

--- rs-lib/src/pack/mod.rs
+++ rs-lib/src/pack/mod.rs
@@ -7,15 +7,14 @@ use std::sync::Arc;
 
 use anyhow::bail;
 use anyhow::Result;
-use deno_ast::SourceRangedForSpanned;
 use deno_ast::apply_text_changes;
 use deno_ast::parse_module;
-use deno_ast::view::DefaultDecl;
 use deno_ast::swc::ast::Id;
 use deno_ast::swc::parser::token::Keyword;
 use deno_ast::swc::parser::token::Token;
 use deno_ast::swc::parser::token::Word;
 use deno_ast::view::AwaitExpr;
+use deno_ast::view::DefaultDecl;
 use deno_ast::view::ImportSpecifier;
 use deno_ast::view::Module;
 use deno_ast::view::ModuleDecl;
@@ -29,10 +28,13 @@ use deno_ast::ModuleSpecifier;
 use deno_ast::ParseParams;
 use deno_ast::ParsedSource;
 use deno_ast::SourceRanged;
+use deno_ast::SourceRangedForSpanned;
 use deno_ast::SourceTextInfo;
 use deno_ast::SourceTextInfoProvider;
 use deno_ast::TextChange;
 use deno_graph::CapturingModuleParser;
+use deno_graph::EsmModule;
+use deno_graph::JsonModule;
 use deno_graph::ModuleGraph;
 use deno_graph::ModuleParser;
 use deno_graph::WalkOptions;
@@ -74,15 +76,25 @@ struct IdReplace {
   module_id: usize,
 }
 
+struct Context<'a> {
+  graph: &'a ModuleGraph,
+  parser: &'a CapturingModuleParser<'a>,
+  module_data: ModuleDataCollection,
+}
+
 pub fn pack(
   graph: &ModuleGraph,
   parser: &CapturingModuleParser,
 ) -> Result<String> {
+  // todo: handle dynamic imports
   let roots = &graph.roots;
   assert_eq!(roots.len(), 1);
-  let mut dynamic_modules: HashSet<ModuleSpecifier> = Default::default();
-  let mut module_data = ModuleDataCollection::default();
-  let mut seen: HashSet<&ModuleSpecifier> = Default::default();
+  let mut context = Context {
+    graph,
+    parser,
+    module_data: ModuleDataCollection::default(),
+  };
+
   let mut ordered_specifiers: Vec<(&ModuleSpecifier, &deno_graph::Module)> =
     Default::default();
 
@@ -97,211 +109,32 @@ pub fn pack(
   for (specifier, _) in modules {
     let module = graph.get(specifier).unwrap();
     let specifier = module.specifier();
-    if !seen.insert(specifier) {
-      continue;
-    }
-
-    if let Some(esm) = module.esm() {
-      ordered_specifiers.push((specifier, module));
-      let parsed_source =
-        parser.parse_module(specifier, esm.source.clone(), esm.media_type)?;
-
-      parsed_source.with_view(|program| {
-        let mut replace_ids = HashMap::new();
-        let module = program.module();
-        for module_item in &module.body {
-          match module_item {
-            ModuleItem::Stmt(stmt) => {
-              let top_level_await = stmt.to::<AwaitExpr>();
-              if top_level_await.is_some() {
-                todo!();
-              }
-            }
-            ModuleItem::ModuleDecl(decl) => match decl {
-              ModuleDecl::Import(import) => {
-                if import.type_only() {
-                  todo!();
-                }
-
-                let value: &str = import.src.value();
-                match graph.resolve_dependency(value, specifier, false) {
-                  Some(dep_specifier) => {
-                    let dep_module_id = module_data.get_mut(&dep_specifier).id;
-                    let range = import.range();
-                    if import.asserts.is_some() {
-                      todo!();
-                    }
-                    for import_specifier in &import.specifiers {
-                      match import_specifier {
-                        ImportSpecifier::Default(default_specifier) => {
-                          replace_ids.insert(
-                            default_specifier.local.to_id(),
-                            format!(
-                              "{}.__pack_default__",
-                              dep_module_id.to_code_string(),
-                            ),
-                          );
-                        }
-                        ImportSpecifier::Namespace(namespace_specifier) => {
-                          replace_ids.insert(
-                            namespace_specifier.local.to_id(),
-                            dep_module_id.to_code_string(),
-                          );
-                        }
-                        ImportSpecifier::Named(named_specifier) => {
-                          replace_ids.insert(
-                            named_specifier.local.to_id(),
-                            format!(
-                              "{}.{}",
-                              dep_module_id.to_code_string(),
-                              named_specifier
-                                .imported
-                                .map(|i| {
-                                  match i {
-                                    ModuleExportName::Str(_) => todo!(),
-                                    ModuleExportName::Ident(ident) => {
-                                      ident.text_fast(module)
-                                    }
-                                  }
-                                })
-                                .unwrap_or_else(|| named_specifier
-                                  .local
-                                  .text_fast(module))
-                            ),
-                          );
-                        }
-                      }
-                    }
-
-                    // remove the import statement
-                    module_data.get_mut(&specifier).text_changes.push(
-                      TextChange {
-                        range: range.as_byte_range(
-                          parsed_source.text_info().range().start,
-                        ),
-                        new_text: String::new(),
-                      },
-                    );
-                  }
-                  None => {
-                    todo!();
-                  }
-                }
-              }
-              ModuleDecl::ExportDefaultDecl(decl) => {
-                let export_keyword = decl.tokens_fast(module).iter().find(|t| t.token == Token::Word(Word::Keyword(Keyword::Export))).unwrap();
-                let default_keyword = export_keyword.range().next_token_fast(module).unwrap();
-                assert_eq!(default_keyword.token, Token::Word(Word::Keyword(Keyword::Default_)));
-                let file_start = parsed_source.text_info().range().start;
-                // remove the "export default" keywords
-                let module_data = module_data.get_mut(&specifier);
-                module_data.text_changes.push(
-                  TextChange {
-                    range: export_keyword.start().as_byte_index(file_start)..default_keyword.end().as_byte_index(file_start),
-                    new_text: String::new(),
-                  },
-                );
-
-                // change the default export to have a different name for the export
-                // since we can't use the word "default"
-                if let DefaultDecl::TsInterfaceDecl(decl) = &decl.decl {
-                    let end = decl.end().as_byte_index(file_start);
-                    module_data.text_changes.push(
-                      TextChange {
-                        range: end..end,
-                        new_text: format!("\nexport interface __pack_default__ extends {} {{}}", decl.id.sym()),
-                      }
-                    )
-                } else {
-                  let maybe_ident = match &decl.decl {
-                    DefaultDecl::Class(decl) => {
-                      decl.ident.as_ref()
-                    }
-                    DefaultDecl::Fn(decl) => {
-                      decl.ident.as_ref()
-                    }
-                    DefaultDecl::TsInterfaceDecl(_) => {
-                      unreachable!();
-                    }
-                  };
-                  match maybe_ident {
-                    Some(ident) => {
-                      let end = decl.end().as_byte_index(file_start);
-                      module_data.text_changes.push(
-                        TextChange {
-                          range: end..end,
-                          new_text: format!("\nexport const __pack_default__ = {};", ident.sym()),
-                        }
-                      )
-                    },
-                    None => {
-                      let start = decl.start().as_byte_index(file_start);
-                      module_data.text_changes.push(
-                        TextChange {
-                          range: start..start,
-                          new_text: "export const __pack_default__ = ".to_string(),
-                        }
-                      )
-                    }
-                  }
-                }
-              }
-              _ => {}
-            },
-          }
-        }
-
-        fn for_each_descendant(
-          node: Node,
-          module_data: &mut ModuleData,
-          replace_ids: &HashMap<Id, String>,
-          module: &Module,
-        ) {
-          for child in node.children() {
-            match child {
-              Node::Ident(ident) => {
-                let id = ident.to_id();
-                if let Some(text) = replace_ids.get(&id) {
-                  module_data.text_changes.push(TextChange {
-                    range: ident
-                      .range()
-                      .as_byte_range(module.text_info().range().start),
-                    new_text: text.clone(),
-                  })
-                }
-              }
-              _ => {
-                for_each_descendant(child, module_data, replace_ids, module);
-              }
-            }
-          }
-        }
-
-        let module_data = module_data.get_mut(specifier);
-        for module_item in &module.body {
-          match module_item {
-            ModuleItem::Stmt(stmt) => {
-              for_each_descendant(
-                stmt.into(),
-                module_data,
-                &replace_ids,
-                module,
-              );
-            }
-            _ => {}
-          }
-        }
-      });
+    match module {
+      deno_graph::Module::Esm(esm) => {
+        ordered_specifiers.push((specifier, module));
+        analyze_esm_module(esm, &mut context)?;
+      }
+      deno_graph::Module::Json(json) => {
+        ordered_specifiers.push((specifier, module));
+        analyze_json_module(json, &mut context);
+      }
+      _ => {
+        todo!();
+      }
     }
   }
 
   let mut final_text = String::new();
   for (specifier, module) in ordered_specifiers.iter().rev() {
-    let module = module.esm().unwrap();
-    let module_data = module_data.get_mut(specifier);
+    let source = match module {
+      deno_graph::Module::Esm(esm) => &esm.source,
+      deno_graph::Module::Json(json) => &json.source,
+      _ => unreachable!(),
+    };
+    let module_data = context.module_data.get_mut(specifier);
     // todo: don't clone
     let module_text =
-      apply_text_changes(&module.source, module_data.text_changes.clone());
+      apply_text_changes(source, module_data.text_changes.clone());
     if !final_text.is_empty() {
       final_text.push('\n');
     }
@@ -319,4 +152,204 @@ pub fn pack(
   }
 
   Ok(final_text)
+}
+
+fn analyze_esm_module(esm: &EsmModule, context: &mut Context) -> Result<()> {
+  let specifier = &esm.specifier;
+  let parsed_source = context.parser.parse_module(
+    specifier,
+    esm.source.clone(),
+    esm.media_type,
+  )?;
+
+  parsed_source.with_view(|program| {
+    let mut replace_ids = HashMap::new();
+    let module = program.module();
+    for module_item in &module.body {
+      match module_item {
+        ModuleItem::Stmt(stmt) => {
+          let top_level_await = stmt.to::<AwaitExpr>();
+          if top_level_await.is_some() {
+            todo!();
+          }
+        }
+        ModuleItem::ModuleDecl(decl) => match decl {
+          ModuleDecl::Import(import) => {
+            if import.type_only() {
+              todo!();
+            }
+
+            let value: &str = import.src.value();
+            match context.graph.resolve_dependency(value, specifier, false) {
+              Some(dep_specifier) => {
+                let dep_module_id =
+                  context.module_data.get_mut(&dep_specifier).id;
+                let range = import.range();
+                for import_specifier in &import.specifiers {
+                  match import_specifier {
+                    ImportSpecifier::Default(default_specifier) => {
+                      replace_ids.insert(
+                        default_specifier.local.to_id(),
+                        format!(
+                          "{}.__pack_default__",
+                          dep_module_id.to_code_string(),
+                        ),
+                      );
+                    }
+                    ImportSpecifier::Namespace(namespace_specifier) => {
+                      replace_ids.insert(
+                        namespace_specifier.local.to_id(),
+                        dep_module_id.to_code_string(),
+                      );
+                    }
+                    ImportSpecifier::Named(named_specifier) => {
+                      replace_ids.insert(
+                        named_specifier.local.to_id(),
+                        format!(
+                          "{}.{}",
+                          dep_module_id.to_code_string(),
+                          named_specifier
+                            .imported
+                            .map(|i| {
+                              match i {
+                                ModuleExportName::Str(_) => todo!(),
+                                ModuleExportName::Ident(ident) => {
+                                  ident.text_fast(module)
+                                }
+                              }
+                            })
+                            .unwrap_or_else(|| named_specifier
+                              .local
+                              .text_fast(module))
+                        ),
+                      );
+                    }
+                  }
+                }
+
+                // remove the import statement
+                context.module_data.get_mut(&specifier).text_changes.push(
+                  TextChange {
+                    range: range
+                      .as_byte_range(parsed_source.text_info().range().start),
+                    new_text: String::new(),
+                  },
+                );
+              }
+              None => {
+                todo!();
+              }
+            }
+          }
+          ModuleDecl::ExportDefaultDecl(decl) => {
+            let export_keyword = decl
+              .tokens_fast(module)
+              .iter()
+              .find(|t| t.token == Token::Word(Word::Keyword(Keyword::Export)))
+              .unwrap();
+            let default_keyword =
+              export_keyword.range().next_token_fast(module).unwrap();
+            assert_eq!(
+              default_keyword.token,
+              Token::Word(Word::Keyword(Keyword::Default_))
+            );
+            let file_start = parsed_source.text_info().range().start;
+            // remove the "export default" keywords
+            let module_data = context.module_data.get_mut(&specifier);
+            module_data.text_changes.push(TextChange {
+              range: export_keyword.start().as_byte_index(file_start)
+                ..default_keyword.end().as_byte_index(file_start),
+              new_text: String::new(),
+            });
+
+            // change the default export to have a different name for the export
+            // since we can't use the word "default"
+            if let DefaultDecl::TsInterfaceDecl(decl) = &decl.decl {
+              let end = decl.end().as_byte_index(file_start);
+              module_data.text_changes.push(TextChange {
+                range: end..end,
+                new_text: format!(
+                  "\nexport interface __pack_default__ extends {} {{}}",
+                  decl.id.sym()
+                ),
+              })
+            } else {
+              let maybe_ident = match &decl.decl {
+                DefaultDecl::Class(decl) => decl.ident.as_ref(),
+                DefaultDecl::Fn(decl) => decl.ident.as_ref(),
+                DefaultDecl::TsInterfaceDecl(_) => {
+                  unreachable!();
+                }
+              };
+              match maybe_ident {
+                Some(ident) => {
+                  let end = decl.end().as_byte_index(file_start);
+                  module_data.text_changes.push(TextChange {
+                    range: end..end,
+                    new_text: format!(
+                      "\nexport const __pack_default__ = {};",
+                      ident.sym()
+                    ),
+                  })
+                }
+                None => {
+                  let start = decl.start().as_byte_index(file_start);
+                  module_data.text_changes.push(TextChange {
+                    range: start..start,
+                    new_text: "export const __pack_default__ = ".to_string(),
+                  })
+                }
+              }
+            }
+          }
+          _ => {}
+        },
+      }
+    }
+
+    fn for_each_descendant(
+      node: Node,
+      module_data: &mut ModuleData,
+      replace_ids: &HashMap<Id, String>,
+      module: &Module,
+    ) {
+      for child in node.children() {
+        match child {
+          Node::Ident(ident) => {
+            let id = ident.to_id();
+            if let Some(text) = replace_ids.get(&id) {
+              module_data.text_changes.push(TextChange {
+                range: ident
+                  .range()
+                  .as_byte_range(module.text_info().range().start),
+                new_text: text.clone(),
+              })
+            }
+          }
+          _ => {
+            for_each_descendant(child, module_data, replace_ids, module);
+          }
+        }
+      }
+    }
+
+    let module_data = context.module_data.get_mut(specifier);
+    for module_item in &module.body {
+      match module_item {
+        ModuleItem::Stmt(stmt) => {
+          for_each_descendant(stmt.into(), module_data, &replace_ids, module);
+        }
+        _ => {}
+      }
+    }
+  });
+  Ok(())
+}
+
+fn analyze_json_module(json: &JsonModule, context: &mut Context) {
+  let module_data = context.module_data.get_mut(&json.specifier);
+  module_data.text_changes.push(TextChange {
+    range: 0..json.source.len(),
+    new_text: format!("export const __pack_default__ = {};", json.source),
+  });
 }
--- rs-lib/tests/integration_test.rs
+++ rs-lib/tests/integration_test.rs
@@ -48,14 +48,13 @@ const myLogger: pack0.Logger = {
 console.log(myLogger.log("Hello"));
 "#;
 
-  assert_eq!(
-    builder.pack().await.unwrap(),
-    expected_output,
-  );
+  assert_eq!(builder.pack().await.unwrap(), expected_output,);
 
   // now try changing mod.ts to use a namespace import
   builder.with_loader(|loader| {
-    loader.add_file("file:///mod.ts", r#"
+    loader.add_file(
+      "file:///mod.ts",
+      r#"
 import * as logger from './logger.ts';
 
 const myLogger: logger.Logger = {
@@ -65,21 +64,21 @@ const myLogger: logger.Logger = {
 }
 
 console.log(myLogger.log("Hello"));
-"#);
+"#,
+    );
   });
 
   // the output should be the same
-  assert_eq!(
-    builder.pack().await.unwrap(),
-    expected_output,
-  );
+  assert_eq!(builder.pack().await.unwrap(), expected_output,);
 }
 
 #[tokio::test]
 async fn test_pack_default_exports() {
   let mut builder = TestBuilder::new();
   builder.with_loader(|loader| {
-    loader.add_file("file:///mod.ts", r#"
+    loader.add_file(
+      "file:///mod.ts",
+      r#"
 import Default1 from './a.ts';
 import Default2 from './b.ts';
 import Default3 from './c.ts';
@@ -91,23 +90,39 @@ console.log(Default2);
 console.log(Default3);
 console.log(Default4);
 console.log(Default5);
-"#);
-    loader.add_file("file:///a.ts", r#"
+"#,
+    );
+    loader.add_file(
+      "file:///a.ts",
+      r#"
 export default interface A {
 }
-"#);
-    loader.add_file("file:///b.ts", r#"
+"#,
+    );
+    loader.add_file(
+      "file:///b.ts",
+      r#"
 export default class B {}
-"#);
-    loader.add_file("file:///c.ts", r#"
+"#,
+    );
+    loader.add_file(
+      "file:///c.ts",
+      r#"
 export default class {}
-"#);
-    loader.add_file("file:///d.ts", r#"
+"#,
+    );
+    loader.add_file(
+      "file:///d.ts",
+      r#"
 export default function d() {}
-"#);
-    loader.add_file("file:///e.ts", r#"
+"#,
+    );
+    loader.add_file(
+      "file:///e.ts",
+      r#"
 export default function() {}
-"#);
+"#,
+    );
   });
 
   let expected_output = r#"// file:///e.ts
@@ -147,8 +162,40 @@ console.log(pack4.__pack_default__);
 console.log(pack5.__pack_default__);
 "#;
 
-  assert_eq!(
-    builder.pack().await.unwrap(),
-    expected_output,
-  );
+  assert_eq!(builder.pack().await.unwrap(), expected_output,);
+}
+
+#[tokio::test]
+async fn test_pack_json_module() {
+  let mut builder = TestBuilder::new();
+  builder.with_loader(|loader| {
+    loader.add_file(
+      "file:///mod.ts",
+      r#"
+import myJson from './data.json' assert {
+  type: "json"
+};
+console.log(myJson.data);
+"#,
+    );
+    loader.add_file(
+      "file:///data.json",
+      r#"{
+  "data": 5
+}"#,
+    );
+  });
+
+  let expected_output = r#"// file:///data.json
+namespace pack0 {
+export const __pack_default__ = {
+  "data": 5
+};
+}
+
+// file:///mod.ts
+console.log(pack0.__pack_default__.data);
+"#;
+
+  assert_eq!(builder.pack().await.unwrap(), expected_output,);
 }
