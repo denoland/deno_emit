04faf903bdacbbc71e2afacb3586f52186f07724
Yacine Hmito <6893840+yacinehmito@users.noreply.github.com>
2023-05-04 04:47:12 +0200

    More import_map tests

--- tests/__snapshots__/import_map/embedded_import_map_takes_precedence_over_file/bundle.js
+++
@@ -1,2 +0,0 @@
-const foo = "foo2";
-console.log(foo);
--- tests/import_map_test.ts
+++ tests/import_map_test.ts
@@ -3,7 +3,7 @@ import { assertEquals } from "https://deno.land/std@0.182.0/testing/asserts.ts";
 import { resolveFixture, runModule, testTranspileAndBundle } from "./utils.ts";
 
 Deno.test({
-  name: "embedded import map",
+  name: "embedded",
   fn: testTranspileAndBundle(
     resolveFixture("import_map/main.ts"),
     {
@@ -23,7 +23,7 @@ Deno.test({
 });
 
 Deno.test({
-  name: "embedded import map with specific base url",
+  name: "embedded with specific base url",
   fn: testTranspileAndBundle(
     resolveFixture("import_map/main.ts"),
     {
@@ -44,7 +44,23 @@ Deno.test({
 });
 
 Deno.test({
-  name: "import map as local file",
+  name: "local file set as relative path",
+  fn: testTranspileAndBundle(
+    resolveFixture("import_map/main.ts"),
+    {
+      importMap: "./testdata/import_map/import_map.json",
+    },
+    async ({ outputFileUrl, denoConfigPath, functionCalled }) => {
+      if (functionCalled === "bundle") {
+        const output = await runModule(outputFileUrl, denoConfigPath);
+        assertEquals(output, "foo\n");
+      }
+    },
+  ),
+});
+
+Deno.test({
+  name: "local file set as absolute path",
   fn: testTranspileAndBundle(
     resolveFixture("import_map/main.ts"),
     {
@@ -60,11 +76,64 @@ Deno.test({
 });
 
 Deno.test({
-  name: "import map as remote file",
+  name: "local file set as file url string",
+  fn: testTranspileAndBundle(
+    resolveFixture("import_map/main.ts"),
+    {
+      importMap: toFileUrl(resolveFixture("import_map/import_map.json"))
+        .toString(),
+    },
+    async ({ outputFileUrl, denoConfigPath, functionCalled }) => {
+      if (functionCalled === "bundle") {
+        const output = await runModule(outputFileUrl, denoConfigPath);
+        assertEquals(output, "foo\n");
+      }
+    },
+  ),
+  // FIXME: Doesn't pass yet.
+  ignore: true,
+});
+
+Deno.test({
+  name: "local file set as file url object",
+  fn: testTranspileAndBundle(
+    resolveFixture("import_map/main.ts"),
+    {
+      importMap: toFileUrl(resolveFixture("import_map/import_map.json")),
+    },
+    async ({ outputFileUrl, denoConfigPath, functionCalled }) => {
+      if (functionCalled === "bundle") {
+        const output = await runModule(outputFileUrl, denoConfigPath);
+        assertEquals(output, "foo\n");
+      }
+    },
+  ),
+});
+
+Deno.test({
+  name: "remote url string",
   fn: testTranspileAndBundle(
     resolveFixture("import_map/main.ts"),
     {
       importMap: "http://localhost:8000/import_map/import_map.json",
+    },
+    async ({ outputFileUrl, denoConfigPath, functionCalled }) => {
+      if (functionCalled === "bundle") {
+        const output = await runModule(outputFileUrl, denoConfigPath);
+        assertEquals(output, "foo\n");
+      }
+    },
+  ),
+  // TODO: Run a local server to be able to test this.
+  ignore: true,
+});
+
+Deno.test({
+  name: "remote url object",
+  fn: testTranspileAndBundle(
+    resolveFixture("import_map/main.ts"),
+    {
+      importMap: new URL("http://localhost:8000/import_map/import_map.json"),
     },
     async ({ outputFileUrl, denoConfigPath, functionCalled }) => {
       if (functionCalled === "bundle") {
